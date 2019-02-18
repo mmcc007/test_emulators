d3661f35ff3408d03a0dde0835f0d2d98ca36099
Maurice McCabe <mmcc007@gmail.com>
2019-02-17 23:11:58 -0800

    Tested env logic

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-SYS=23
+SYS=25
 ANDROID_ABI="google_apis;armeabi-v7a"
-    #if [[ "$SYS$ANDROID_ABI" == "25google_apis;armeabi-v7a" || "$SYS$ANDROID_ABI" == "24google_apis;armeabi-v7a"]]; then
-    if [[ "$SYS$ANDROID_ABI" == "25google_apis;armeabi-v7a"
-       || "$SYS$ANDROID_ABI" == "24google_apis;armeabi-v7a" ]]; then
+    if [[ "${SYS}${ANDROID_ABI}" == "25google_apis;armeabi-v7a"
+       || "${SYS}${ANDROID_ABI}" == "24google_apis;armeabi-v7a" ]]; then
       GPU='swiftshader'
     else
       GPU='off'
