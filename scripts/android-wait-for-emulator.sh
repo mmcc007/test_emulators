ad278e29ebf6e1f1f584e8aeca0fc2310789dfc2
Maurice McCabe <mmcc007@users.noreply.github.com>
2019-02-18 16:39:03 -0800

    Added xvfb (X Virtual Framebuffer) to linux and start emulators with no GPU (#1)
    * turned-off emulator gpu
    
    * Removed unlock screen command
    
    * Changed emulator parameters according to env
    
    * Started xvfb on linux only
    
    * Added error condition to wait-for-emulator

--- scripts/android-wait-for-emulator.sh
+++ scripts/android-wait-for-emulator.sh
@@ -11,8 +11,9 @@ timeout_in_sec=600 # 10 minutes
 
 until [[ "$bootanim" =~ "stopped" ]]; do
   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+#echo bootanim=\`$bootanim\`
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
-    || "$bootanim" =~ "running" ]]; then
+    || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
     let "failcounter += 1"
     echo "Waiting for emulator to start"
     if [[ $failcounter -gt timeout_in_sec ]]; then
